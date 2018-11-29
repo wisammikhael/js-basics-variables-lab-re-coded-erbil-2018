c706d9302b8b0bfb343fea2d457a0aff93a64e0a
matrixlife1976 <matrixlife1976@gmail.com>
2018-11-29 13:55:57 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,10 +1,4 @@
-const expect = chai.expect;
 
-describe('index.js', function () {
-  describe('companyName', function () {
-    it('is set as Scuber', function () {
-      expect(companyName).to.equal('Scuber');
-    });
 
     it('raises error if the companyName is changed', function () {
       expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
